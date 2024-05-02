4e41f69901eb4f373f4087fe4ec8f83d031a6c85
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2024-05-02 14:25:18 +0700

    add: initial work on basetypes

---
+++ src/class/node.ts
@@ -0,0 +1,94 @@
+class node {
+  private _position: Vector3 = new Vector3();
+  private _rotation: Vector3 = new Vector3();
+  private _scale: Vector3 = new Vector3(1, 1, 1);
+  private _localMatrix: M4 = M4.identity();
+  private _worldMatrix: M4 = M4.identity();
+  private _parent?: Node;
+  private _children: Node[] = [];
+  visible = true;
+
+  // Public getter, prevent re-instance new object
+  get position() {
+    return this._position;
+  }
+  get rotation() {
+    return this._rotation;
+  }
+  get scale() {
+    return this._scale;
+  }
+  get parent() {
+    return this._parent;
+  }
+  get localMatrix() {
+    return this._localMatrix;
+  }
+  get worldMatrix() {
+    return this._worldMatrix;
+  }
+  get children() {
+    return this._children;
+  }
+
+  // Public setter
+  // Should update world matrix if parent changed
+  set parent(parent) {
+    if (this._parent !== parent) {
+      this._parent = parent;
+      this.computeWorldMatrix(false, true);
+    }
+  }
+
+  computeLocalMatrix() {
+    this._localMatrix = M4.mul(
+      M4.translation3d(this._position),
+      M4.rotation3d(this._rotation),
+      M4.scale3d(this._scale)
+    );
+  }
+
+  computeWorldMatrix(updateParent = true, updateChildren = true) {
+    // If updateParent, update world matrix of our ancestors
+    // (.parent, .parent.parent, .parent.parent.parent, ...)
+    if (updateParent && this.parent)
+      this.parent.computeWorldMatrix(true, false);
+    // Update this node
+    this.computeLocalMatrix();
+    if (this.parent) {
+      this._worldMatrix = M4.mul(this.parent.worldMatrix, this._localMatrix);
+    } else {
+      this._worldMatrix = this._localMatrix.clone();
+    }
+    // If updateChildren, update our children
+    // (.children, .children.children, .children.children.children, ...)
+    if (updateChildren)
+      for (let i = 0; i < this._children.length; i++)
+        this._children[i].computeWorldMatrix(false, true);
+  }
+
+  /**
+   * Tambah node sebagai child dari node ini.
+   *
+   * Jika node sudah memiliki parent, maka node akan
+   * dilepas dari parentnya terlebih dahulu.
+   */
+  add(node: Node): Node {
+    if (node.parent !== this) {
+      node.removeFromParent();
+      node.parent = this;
+    }
+    this.children.push(node);
+    return this;
+  }
+
+  remove(node: Node) {
+    // TODO: hapus node dari this.children (jangan lupa set node.parent = null)
+    return this;
+  }
+
+  removeFromParent() {
+    if (this.parent) this.parent.remove(this);
+    return this;
+  }
+}
---
+++ src/types/m4.ts
@@ -0,0 +1,7 @@
+class M4 {
+  static identity(): M4 {
+    var identityMatrix = new M4();
+
+    return identityMatrix;
+  }
+}
