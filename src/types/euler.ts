ffc760cc3972bd851bbaab4d61314b1f639410d9
Tansil011019 <13521054@std.stei.itb.ac.id>
2024-05-04 02:35:16 +0700

    feat: add implementation of quaternion to euler and m4 to euler

--- src/types/euler.ts
+++ src/types/euler.ts
@@ -1,4 +1,5 @@
 import M4 from "./m4";
+import { Quaternion } from "./quaternion";
 import Vector3 from "./vector3";
 
 class Euler {
@@ -63,11 +64,73 @@ class Euler {
     return this;
   }
 
-  setFromRotationMatrix(m: M4, order?: string): this {
+  setFromRotationMatrix(m: M4, quadran: number, order?: string): this {
+    // implementation of order will always XYZ if not needed to consider the order
+    // quadran === 14 => quadran 1 and quadran 4
+    // quadran === 23 => quadran 2 and quadran 3
+
+    const matrix = m.matrix.map((row) => [...row]);
+    const rotationMatix = matrix.slice(0, 3).map((row) => row.slice(0, 3));
+
+    let theta1,
+      theta2,
+      psi1,
+      psi2,
+      phi1 = 0,
+      phi2;
+
+    if (rotationMatix[3][1] != -1 && rotationMatix[3][1] != 1) {
+      theta1 = -Math.asin(rotationMatix[3][1]);
+      theta2 = Math.PI - theta1;
+      psi1 = Math.atan2(rotationMatix[3][2], rotationMatix[3][3]);
+      psi2 = Math.atan2(-rotationMatix[3][2], -rotationMatix[3][3]);
+      phi1 = Math.atan2(rotationMatix[2][1], Math.cos(theta1) * Math.cos(psi1));
+      phi2 = Math.atan2(
+        -rotationMatix[2][1],
+        Math.cos(theta2) * Math.cos(psi2)
+      );
+    } else {
+      if (rotationMatix[3][1] === -1) {
+        theta1 = Math.PI / 2;
+        psi1 = phi1 + Math.atan2(rotationMatix[1][2], rotationMatix[1][3]);
+      } else {
+        theta1 = -Math.PI / 2;
+        psi1 = -phi1 + Math.atan2(-rotationMatix[1][2], -rotationMatix[1][3]);
+      }
+    }
+
+    if (!theta2 && quadran === 14) {
+      this.x = theta1
+      this.y = psi1
+      this.z = phi1
+    } else {
+      this.x = theta2!!
+      this.y = psi2!!
+      this.z = phi2!!
+    }
+
+    this.order = order ?? this.order
+
     return this;
   }
 
   setFromQuaternion(q: Quaternion, order?: string): this {
+    // implementation of order will always XYZ if not needed to consider the order
+
+    const sinr_cosp = 2 * (q.w * q.x + q.y * q.z)
+    const cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y)
+    this.x = Math.atan2(sinr_cosp, cosr_cosp)
+
+    const sinp = Math.sqrt(1 + 2 * (q.w * q.y - q.x * q.z))
+    const cosp = Math.sqrt(1 - 2 * (q.w * q.y - q.x * q.z))
+    this.y =  2 * Math.atan2(sinp, cosp) - Math.PI / 2
+
+    const siny_cosp = 2 * (q.w * q.z + q.x * q.y)
+    const cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
+    this.z = Math.atan2(siny_cosp, cosy_cosp)
+
+    this.order = order ?? this.order
+
     return this;
   }
 
@@ -75,7 +138,7 @@ class Euler {
     this.x = vector.x;
     this.y = vector.y;
     this.z = vector.z;
-    this.order = order ?? this.order
+    this.order = order ?? this.order;
     return this;
   }
 
@@ -88,9 +151,5 @@ class Euler {
     array[offset + 2] = this.z;
     array[offset + 3] = this.order;
     return array;
-  }
-
-  applyEuler(target: Quaternion| Vector3| M4) {
-
   }
 }
