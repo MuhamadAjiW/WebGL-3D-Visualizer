0d41a10e1eb73ad4c59bc77344b5806648bb0009
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-05-03 10:33:25 +0700

    refactor: buffer uniform type

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { WebGLUtil } from './util/webgl-util';
 import { createUniformSetters, setUniforms } from './types/webgl-setters-uniform';
 import { createAttributeSetters, setAttributes } from './types/webgl-setters-attribute';
 import { BufferAttribute } from './class/geometry';
+import { BufferUniform } from './class/uniform';
 
 const canvas: HTMLCanvasElement = document.querySelector<HTMLCanvasElement>('#webgl-canvas') as HTMLCanvasElement;
 const gl: WebGLRenderingContext = canvas.getContext("webgl") as WebGLRenderingContext;
@@ -35,56 +36,104 @@ let programInfo: ProgramInfo = {
 
 // TODO: Delete, this is for testing purposes
 const dummyUniformsData = {
-  u_projectionMatrix: new Float32Array([
-    1, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    0, 0, 0, 1
-  ]),
-  u_world: new Float32Array([
-    1, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    0, 0, 0, 1
-  ]),
-  u_lightWorldPos: new Float32Array([1.0, 2.0, 3.0]),
-  u_viewInverse: new Float32Array(16),
-  u_worldInverseTranspose: new Float32Array(16),
-  u_lightColor: [1.0, 1.0, 1.0, 1.0],
-  u_ambient: [0.2, 0.2, 0.2, 1.0],
-  u_diffuse: [0.5, 0.5, 0.5, 1.0],
-  u_specular: [1.0, 1.0, 1.0, 1.0],
+  u_projectionMatrix: new BufferUniform(
+    new Float32Array([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]),
+    1, gl.FLOAT_MAT4
+  ),
+  u_world: new BufferUniform(
+    new Float32Array([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]),
+    1, gl.FLOAT_MAT4
+  ),
+  u_lightWorldPos: new BufferUniform(
+    new Float32Array([
+      1, 0, 0
+    ]),
+    1, gl.FLOAT_VEC3
+  ),
+  u_viewInverse: new BufferUniform(
+    new Float32Array([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]),
+    1, gl.FLOAT_MAT4
+  ),
+  u_worldInverseTranspose: new BufferUniform(
+    new Float32Array([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]),
+    1, gl.FLOAT_MAT4
+  ),
+  u_lightColor: new BufferUniform(
+    new Float32Array([
+      1, 1, 1, 1
+    ]),
+    1, gl.FLOAT_VEC4
+  ),
+  u_ambient: new BufferUniform(
+    new Float32Array([
+      1, 1, 1, 1
+    ]),
+    1, gl.FLOAT_VEC4
+  ),
+  u_diffuse: new BufferUniform(
+    new Float32Array([
+      1, 1, 1, 1
+    ]),
+    1, gl.FLOAT_VEC4
+  ),
+  u_specular: new BufferUniform(
+    new Float32Array([
+      1, 1, 1, 1
+    ]),
+    1, gl.FLOAT_VEC4
+  ),
   u_shininess: 32.0,
   u_specularFactor: 0.5,
 };
 
-var positions = [
-  0, 0, 0, 1,
-  1, 0, 0, 1,
-  0.5, 1, 0, 1,
-  0.5, 1, 0, 1
-]
 const dummyAttributesData = {
   a_position: new BufferAttribute(
-    new Float32Array(positions), 4, 
+    new Float32Array([
+      0, 0, 0, 1,
+      1, 0, 0, 1,
+      0.5, 1, 0, 1,
+      0.5, 1, 0, 1
+    ]), 4, 
     {dtype: gl.FLOAT, normalize: false, stride: 0, offset: 0} 
   ),
-  a_normal: [
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0
-  ],
-  a_texcoord: [
-    0.0, 0.0,
-    0.0, 1.0,
-    1.0, 1.0,
-    1.0, 1.0,
-    1.0, 0.0,
-    0.0, 0.0
-  ],
+  a_normal: new BufferAttribute(
+    new Float32Array([
+      0, 0, 0, 1,
+      1, 0, 0, 1,
+      0.5, 1, 0, 1,
+      0.5, 1, 0, 1
+    ]), 4, 
+    {dtype: gl.FLOAT, normalize: false, stride: 0, offset: 0} 
+  ),
+  a_texcoord: new BufferAttribute(
+    new Float32Array([
+      0, 0, 0, 1,
+      1, 0, 0, 1,
+      0.5, 1, 0, 1,
+      0.5, 1, 0, 1
+    ]), 4, 
+    {dtype: gl.FLOAT, normalize: false, stride: 0, offset: 0} 
+  ),
 };
 
 gl.useProgram(program);
--- src/types/webgl-program-info.ts
+++ src/types/webgl-program-info.ts
@@ -2,9 +2,9 @@ import { AttributeMapSetters } from "./webgl-setters-attribute";
 import { UniformMapSetters } from "./webgl-setters-uniform";
 
 type ProgramInfo = {
-    program: WebGLProgram,
-    uniformSetters: UniformMapSetters,
-    attributeSetters: AttributeMapSetters,
+  program: WebGLProgram,
+  uniformSetters: UniformMapSetters,
+  attributeSetters: AttributeMapSetters,
 };
 
 export type { ProgramInfo };
