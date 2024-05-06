652ed85afcb93a90122d8140a1ff287ceb95e893
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2024-05-06 16:05:47 +0700

    feat: distance, orbit node belom selesai

--- src/class/camera.ts
+++ src/class/camera.ts
@@ -1,9 +1,19 @@
 import Object3d from "./object3d";
 import M4 from "../base-types/m4";
+import Vector3 from "../base-types/vector3";
 
 class Camera extends Object3d {
     protected _projectionMatrix = M4.identity();
     private _invWorldMatrix = M4.identity();
+    private _distance: number = 10;
+    private _orbitNode: Object3d;
+
+    constructor() {
+        super();
+        this._orbitNode = new Object3d();
+        this.add(this._orbitNode);
+        this.position = new Vector3(0, 0, -this._distance);
+    }
 
 
     computeWorldMatrix() {
@@ -15,7 +25,6 @@ class Camera extends Object3d {
     get viewProjectionMatrix() {
         this.computeWorldMatrix();
         return M4.multiply(this.projectionMatrix, this._invWorldMatrix);
-        // return this.projectionMatrix.premul(this._invWorldMatrix);
     }
 
     get projectionMatrix() {
@@ -24,6 +33,15 @@ class Camera extends Object3d {
 
     computeProjectionMatrix() {
         throw new Error("Camera.computeProjectionMatrix() must be implemented in derived classes.");
+    }
+
+    get distance() {
+        return this._distance;
+    }
+
+    set distance(value: number) {
+        this._distance = value;
+        this.position = new Vector3(0, 0, -this._distance);
     }
 }
 
