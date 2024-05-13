707ee201b94071724580924dd7ac5461d0d12fd6
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2024-05-13 15:42:16 +0700

    refactor: fix bug

--- src/class/camera.ts
+++ src/class/camera.ts
@@ -1,12 +1,15 @@
 import Object3d from "./object3d";
 import M4 from "../base-types/m4";
 import Vector3 from "../base-types/vector3";
+import { MathUtil } from "../util/math-util";
 
 class Camera extends Object3d {
     protected _projectionMatrix = M4.identity();
     private _invWorldMatrix = M4.identity();
     private _distance: number = 10;
     private _orbitNode: Object3d;
+    public angle: number = 0;
+    private _cameraMatrix = M4.identity();
 
     constructor() {
         super();
@@ -15,24 +18,40 @@ class Camera extends Object3d {
         this.position = new Vector3(0, 0, -this._distance);
     }
 
+    radToDeg(r: number) {
+        return r * 180 / Math.PI;
+    }
+
+    degToRad(d: number) {
+        return d * Math.PI / 180;
+    }
+
+    computeCameraMatrix() {
+        var matrix = M4.yRotation(MathUtil.DegreesToRad(this.angle));
+        var translationMatrix = M4.translation3d(new Vector3(0,0,3));
+        this._cameraMatrix = M4.multiply(matrix, translationMatrix);
+    }
+
+    computeViewMatrix() {
+        this.computeCameraMatrix();
+        return this._cameraMatrix.inverse();
+    }
 
     computeWorldMatrix() {
         super.computeWorldMatrix();
         this._invWorldMatrix = this.worldMatrix.inverse();
     }
 
+    computeProjectionMatrix() {
+        throw new Error("Camera.computeProjectionMatrix() must be implemented in derived classes.");
+    }
 
     get viewProjectionMatrix() {
-        this.computeWorldMatrix();
-        return M4.multiply(this.projectionMatrix, this._invWorldMatrix);
+        return M4.multiply(this.projectionMatrix, this.computeViewMatrix());
     }
 
     get projectionMatrix() {
         return this._projectionMatrix;
-    }
-
-    computeProjectionMatrix() {
-        throw new Error("Camera.computeProjectionMatrix() must be implemented in derived classes.");
     }
 
     get distance() {
