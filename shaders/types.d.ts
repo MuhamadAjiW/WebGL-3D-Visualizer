07a2429de047af057dcd3f4e498d7c0444e211ed
Tansil011019 <13521054@std.stei.itb.ac.id>
2024-05-13 20:23:20 +0700

    refactor: tidy types

--- shaders/types.d.ts
+++ shaders/types.d.ts
@@ -1,10 +1,9 @@
 declare module "*.vert?raw" {
-    const content: string;
-    export default content;
-  }
-  
-  declare module "*.frag?raw" {
-    const content: string;
-    export default content;
-  }
-  
+  const content: string;
+  export default content;
+}
+
+declare module "*.frag?raw" {
+  const content: string;
+  export default content;
+}
