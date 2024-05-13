ee4fda880615d5fe34988f1db480345b601d94a3
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-05-13 21:06:55 +0700

    Updated RenderComponent

--- components/render/RenderComponent.tsx
+++ components/render/RenderComponent.tsx
@@ -20,6 +20,7 @@ import M4 from "@/libs/base-types/m4";
 import PersepectiveCamera from "@/libs/class/persepective-camera";
 import { MathUtil } from "@/libs/util/math-util";
 import { useEffect, useRef } from "react";
+import { CubeGeometry } from "@/libs/class/geometry/cube-geometry";
 
 interface RenderedComponentProps {}
 
@@ -74,91 +75,66 @@ const RenderedComponent: React.FC<RenderedComponentProps> = ({}) => {
         2000
       );
       camera.position = new Vector3(0, 0, 0);
-      camera.angle = 70;
       console.log(M4.flatten(camera.viewProjectionMatrix));
       console.log(camera);
-
+      
       // TODO: Delete, this is for testing purposes
       const dummyUniformsData = {
         // Camera
         u_projectionMatrix: new BufferUniform(
           new Float32Array(M4.flatten(camera.viewProjectionMatrix)),
-          1,
+          16,
           gl.FLOAT_MAT4
         ),
         // Node
         u_world: new BufferUniform(
-          new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]),
-          1,
-          gl.FLOAT_MAT4
+          new Float32Array([
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+          ]),
+          16, gl.FLOAT_MAT4
         ),
         // Light
         u_lightWorldPos: new BufferUniform(
           new Float32Array([1, 0, 0]),
-          1,
+          3,
           gl.FLOAT_VEC3
         ),
         // Camera
         u_viewInverse: new BufferUniform(
-          new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]),
-          1,
-          gl.FLOAT_MAT4
+          new Float32Array([
+            1, 0, 0, 0, 
+            0, 1, 0, 0, 
+            0, 0, 1, 0, 
+            0, 0, 0, 1
+          ]),
+          16, gl.FLOAT_MAT4
         ),
         // Node
         u_worldInverseTranspose: new BufferUniform(
-          new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]),
-          1,
-          gl.FLOAT_MAT4
+          new Float32Array([
+            1, 0, 0, 0, 
+            0, 1, 0, 0, 
+            0, 0, 1, 0, 
+            0, 0, 0, 1
+          ]),
+          16, gl.FLOAT_MAT4
         ),
         // Light
         u_lightColor: new BufferUniform(
           new Float32Array([1, 1, 1, 1]),
-          1,
+          4,
           gl.FLOAT_VEC4
         ),
       };
-
-      const dummyAttributesData = {
-        // Geometry
-        a_position: new BufferAttribute(
-          new Float32Array([
-            -0.5, -0.5, 0, -0.5, 0.5, 0, 0.5, -0.5, 0, -0.5, 0.5, 0, 0.5, 0.5,
-            0, 0.5, -0.5, 0,
-          ]),
-          3
-        ),
-        // Geometry
-        a_normal: new BufferAttribute(
-          new Float32Array([
-            -0.5, -0.5, 0, -0.5, 0.5, 0, 0.5, -0.5, 0, -0.5, 0.5, 0, 0.5, 0.5,
-            0, 0.5, -0.5, 0,
-          ]),
-          3,
-          { dtype: gl.FLOAT, normalize: false, stride: 0, offset: 0 }
-        ),
-        // Geometry
-        // Note: 0,0 is top left
-        a_texcoord: new BufferAttribute(
-          new Float32Array([0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1]),
-          2,
-          { dtype: gl.FLOAT, normalize: false, stride: 0, offset: 0 }
-        ),
-      };
-
+      
       gl.useProgram(program);
-
+      
       const scene = new Scene();
-      const geometry = new PlaneGeometry(1, 1);
+      const geometry = new CubeGeometry(2);
 
-      geometry.setAttribute(
-        AttributeKeys.POSITION,
-        dummyAttributesData.a_position
-      );
-      geometry.setAttribute(
-        AttributeKeys.TEXTURE_COORDS,
-        dummyAttributesData.a_texcoord
-      );
-      geometry.setAttribute(AttributeKeys.NORMAL, dummyAttributesData.a_normal);
       // const material = new BasicMaterial({color:new Color(0xff00ffff)});
       const texture = await TextureLoader.load("res/f-texture.png");
       const material = new PhongMaterial({
@@ -172,13 +148,19 @@ const RenderedComponent: React.FC<RenderedComponentProps> = ({}) => {
       const mesh = new Mesh(geometry, material);
       scene.add(mesh);
 
-      WebGLUtils.setUniforms(programInfo, dummyUniformsData);
-      renderer.render(scene, null);
-
-      // Snippet to log uniform/attribute within webgl
-      // const loc = gl.getUniformLocation(programInfo.program, UniformKeys.SPECULAR);
-      // const uniform = gl.getUniform(programInfo.program, loc!);
-      // console.log(uniform)
+      let degrees = 0;
+      const inc = 1;
+      function render (){
+        degrees += inc;
+        camera.setOrbitControl(degrees, degrees);
+        dummyUniformsData.u_projectionMatrix.set(0, M4.flatten(camera.viewProjectionMatrix))
+        WebGLUtils.setUniforms(programInfo, dummyUniformsData);
+      
+        renderer.render(scene, null);
+        requestAnimationFrame(render);
+      }
+      render();
+      
 
       console.log("Done");
     };
